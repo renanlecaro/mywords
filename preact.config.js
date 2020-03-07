5a701b52c0f66a35d1411c61f060e92876dbbc6c
Renan LE CARO <renan.lecaro@gmail.com>
2020-03-07 13:51:34 +1000

    Trying netlify. Why is it so hard to auto build and deploy a static site

--- preact.config.js
+++
@@ -1,7 +0,0 @@
-export default function (config, env, helpers) {
-  /** you can change config here **/
-  env.ASSETS = "/mywords/assets/";
-  if(env.production){
-    config.output.publicPath = "/mywords/";
-  }
-}
