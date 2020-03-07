2644523de6d19ff09ec854b3d65d371a8c154499
Renan LE CARO <renan.lecaro@gmail.com>
2020-03-07 13:29:55 +1000

    Auto: untracked files at build time

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 git diff-index --quiet HEAD -- || git commit -am "Auto: untracked files at build time";
 set -e
-preact build --no-prerender
+npm run build
 git co gh-pages
 find build -maxdepth 1 -mindepth 1 -exec mv {} . \;
-git diff-index --quiet HEAD -- || git commit -am "Build"; 
+git diff-index --quiet HEAD -- || git commit -am "Build";
 git push -f
 git co master
