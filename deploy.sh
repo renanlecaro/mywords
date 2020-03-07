ab0c3f3206772272099f93cbc84d2a62692b9758
Renan LE CARO <renan.lecaro@gmail.com>
2020-03-07 13:33:39 +1000

    Empty folder after before moving build

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ git diff-index --quiet HEAD -- || git commit -am "Auto: untracked files at build
 set -e
 npm run build
 git co gh-pages
+find . \! -name 'build' -delete
 find build -maxdepth 1 -mindepth 1 -exec mv {} . \;
 git diff-index --quiet HEAD -- || git commit -am "Build";
 git push -f
