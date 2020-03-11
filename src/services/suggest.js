b75677daa149f52e846f2a761ce5e62afce5c0f3
Renan LE CARO <renan.lecaro@gmail.com>
2020-03-11 18:57:01 +1000

    Added lots of words

--- src/services/suggest.js
+++ src/services/suggest.js
@@ -1,16 +1,18 @@
 
-import list from './dictionary'
+import {forAutoAdd,forAutocomplete} from './dictionary'
 import {getListOfRussianWords} from "./trainer";
 import {sameish} from "./sameish";
 
+console.log({forAutocomplete, forAutoAdd})
+
 export function getWordToAddToList(){
   const reject=getListOfRussianWords()
-  return list.find(word=>!reject.find(rejected=>sameish(rejected, word.to)))
+  return forAutoAdd.find(word=>!reject.find(rejected=>sameish(rejected, word.to)))
 }
 
 export function suggestions(search='', cb) {
   const reject=getListOfRussianWords()
-  const nodupes=list.filter(word=>!reject.find(rejected=>sameish(rejected, word.to)))
+  const nodupes=forAutocomplete.filter(word=>!reject.find(rejected=>sameish(rejected, word.to)))
   search=search.trim().toLowerCase()
   if(!search) return []
     return firstX(nodupes, 5, wordMatch(search)  ,cb
