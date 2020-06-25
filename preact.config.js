43fc1abb548a1e5be8a4360e9e8452194d4437af
Renan LE CARO <renan.lecaro@gmail.com>
2020-06-25 14:14:29 +1000

    Expose full env to build script

--- preact.config.js
+++ preact.config.js
@@ -1,8 +1,11 @@
 const preactCliSvgLoader = require("preact-cli-svg-loader");
+const webpack = require("webpack");
 
 export default {
   webpack(config, env, helpers, options) {
-    config.node.process = "mock";
+    // config.node.process = "mock";
     preactCliSvgLoader(config, helpers);
+
+    config.plugins.push(new webpack.DefinePlugin(process.env));
   },
 };
